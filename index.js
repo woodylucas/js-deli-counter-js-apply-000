10d57aaf5c4e003ef1b84670cd0ca80521b77ba0
Woody Lucas <wlucas0311@gmail.com>
2018-11-10 01:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function nowServing( katzDeliLine ) {
  function currentLine( katzDeliLine ) {
    // conditional statement 
    if (katzDeliLine.length === 0) {
-     
+     return "The line is currently empty."
    }
  }
