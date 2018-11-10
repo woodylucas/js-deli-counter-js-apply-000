a93684ec9430467288f4fc1d1700958c73b1f83b
Woody Lucas <wlucas0311@gmail.com>
2018-11-10 01:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,7 @@ function nowServing( katzDeliLine ) {
    // conditional statement 
    if (katzDeliLine.length === 0) {
      return "The line is currently empty.";
-   } else 
+   } else {
+     return "The line is currently: 1. " + katzDeliLine[0] + ", 2. " + katzDeliLine[1] + ", 3. " + katzDeliLine[2];
+   }
  }
