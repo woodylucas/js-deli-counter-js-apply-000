c89ac0f62661fc02fa96fe5de23c5eca245dd501
Woody Lucas <wlucas0311@gmail.com>
2018-11-10 01:15:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
-  return "Welcome, " + person + ". You are number " + person + " in line."
+  return "Welcome, " + person + ". You are number " + person + " in line.";
 }
